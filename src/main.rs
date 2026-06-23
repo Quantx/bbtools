5f64d9b4e8f06d678e299c4a37054accc58f338d
Samuel Deutsch <sam.e.deutsch@gmail.com>
2026-06-22 21:37:14 -0700

    Update usage text

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: ./bbtools <game_directory_path>");
+        eprintln!("Usage: ./bbtools <default.xbe|game_disk.iso>");
         return Ok(());
     }
 
